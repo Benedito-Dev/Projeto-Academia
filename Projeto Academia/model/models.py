7a69d1ad62c72e616ef011149cb4077bc43a6296
Benedito-Dev <beneditobittencourtt@gmail.com>
2024-10-25 14:14:06 -0300

    wip

--- Projeto Academia/model/models.py
+++ Projeto Academia/model/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -7,7 +8,28 @@ class Cliente(Base):
     __tablename__ = 'clientes'
     
     # Campos da tabela 'clientes'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String)
+    email = Column(String)
+    senha = Column(String)
+    telefone = Column(String)
+    endereco = Column(String)
+    cpf = Column(String)
+    data_de_nascimento = Column(Date)
+    instrutor_id = Column(Integer, ForeignKey('Instrutores.id'))  # Chave estrangeira para o instrutor
+
+    # Relacionamento com a tabela Instrutores
+    instrutor = relationship("Instrutores", back_populates="clientes")
+
+    def __repr__(self):
+        return f'<Cliente {self.nome}>'
+
+# Instrutores
+class Instrutores(Base):
+    __tablename__ = 'instrutores'
+    
+    # Campos da tabela 'Instrutores'
+    id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String)
     email = Column(String)
     senha = Column(String)
