ff2e3fc26c2227c91acc16b79656d4b1a7459aa6
Benedito-Dev <beneditobittencourtt@gmail.com>
2024-11-30 18:29:54 -0300

    Sprint-2

--- Projeto Academia/repository/repositories.py
+++ Projeto Academia/repository/repositories.py
@@ -249,18 +249,6 @@ class ClienteRepository():
             return administrador  # Retorna o administrador se encontrado
 
         return None  # Se não encontrar em nenhuma tabela, retorna None
-        if cliente:
-            return cliente  # Retorna o cliente se encontrado
-
-        instrutor = self.session.query(Instrutores).filter_by(cpf=cpf).first()
-        if instrutor:
-            return instrutor  # Retorna o instrutor se encontrado
-
-        administrador = self.session.query(Administradores).filter_by(cpf=cpf).first()
-        if administrador:
-            return administrador  # Retorna o administrador se encontrado
-
-        return None  # Se não encontrar em nenhuma tabela, retorna None
     
     def consultar_email(self, email):
         # Buscar o e-mail nas três tabelas
